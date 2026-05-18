9a307397cf04666739b5e47e689e93a05a2ed45b
Nykhanomjan <siwarat.ajar@gmail.com>
2026-05-19 01:23:16 +0700

    add payload Knocking

--- Payload_PortKnocking.py
+++ Payload_PortKnocking.py
@@ -7,3 +7,62 @@
 # ถ้าเชื่อมต่อสำเร็จ ให้ส่ง Payload เป็นรหัส Base64 (สมมติว่าเป็นข้อความ base64_encoded_shell) ไปที่พอร์ตนั้น แล้วรอรับข้อความ "PWNED" ตอบกลับมา
 
 
+
+import time
+import socket
+import base64
+
+ip = "10.10.10.99"
+ports = ['1337','7331','8080']
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.settimeout(0.2)
+# s.connect(ip,1337)
+# s.close()
+# time.sleep(0.2)
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.settimeout(0.2)
+# s.connect(ip,7331)
+# s.close()
+# time.sleep(0.2)
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.settimeout(0.2)
+# s.connect(ip,8080)
+# s.close()
+# time.sleep(0.2)
+
+
+for port in ports:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.5)
+        s.connect(ip,port)
+        s.close()
+        time.sleep(0.2) #delay ระหว่าง port
+    except Exception as e:
+        print(f"[-] failed to connect port {port}")
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.2)
+    s.connect(ip,9999)
+
+    payload = "base64_encoded_shell"
+
+    payload_encode = base64.b64encode(payload.encode("utf-8")) #s.send() รับข้อมูลที่เป็น byte เท่านั้น เลยไม่ต้อง encode
+    s.send(payload_encode +b'\r\n')
+    response = s.recv(1024)
+except Exception as e:
+    print("[-] failed to connect port 9999")
+
+if response.decode('utf-8').strip() =='PWNED' :
+    print("[!!!] Successfully connected, welcome!")
+else:
+    print("[-] failed to connect.")
+
+
+
+
+
+
